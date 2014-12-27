026a3483d38565142c6fe300ac9f792df66c74d4
Zandor300 <info@zsinfo.nl>
2014-12-27 12:52:40 +0100

    IT STILL DOESN'T WORK!!!!

---
+++ src/meta/connect.php
@@ -0,0 +1,14 @@
+<?php
+	$server = "192.168.1.106";
+	$username = "git";
+	$password = "gitweb";
+	$database = "gitmap";
+
+	$connection = mysqli_connect($server, $username,  $password, $database);
+
+	if(!$connection) {
+	    exit('Error: could not establish database connection: ' . mysqli_error($connection));
+	} else if(isset($_GET['debug'])) {
+		echo('Connection to database is established!<br>');
+	}
+?>
